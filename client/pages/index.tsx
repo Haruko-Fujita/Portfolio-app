fce26a3ebdad320e240218d137917eb7398a76e5
Haruko-Fujita <haru.com.hf@gmail.com>
2023-03-24 01:42:44 +0900

    tailwindcssの追加

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <meta charSet="utf-8" />
         <title>portfolio</title>
